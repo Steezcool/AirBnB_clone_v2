87867a15b094011bbe9e9500fe82489398769a8e
Peter McMillan <132352243+Steezcool@users.noreply.github.com>
2024-01-10 17:40:41 +0200

    Update IPMAN

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -7,7 +7,7 @@ from fabric.api import local
 from fabric.api import put
 from fabric.api import run
 
-env.hosts = ["104.196.168.90", "35.196.46.172"]
+env.hosts = ["18.204.15.30", "54.209.107.55"]
 
 
 def do_pack():
